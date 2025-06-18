43725f2f2c98c9e09401be772d4d45b99a54fc7d
sakamw <shanmwangi2020@gmail.com>
2025-06-18 16:23:39 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { BiSolidDrink } from "react-icons/bi";
+function Header() {
+  return (
+    <header className="header">
+      <BiSolidDrink />
+    </header>
+  );
+}
+
+export default Header;
